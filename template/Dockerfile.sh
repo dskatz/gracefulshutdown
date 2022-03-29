26c17e2d55ecbb36802e68bb9763465d8ee9ff1b
David Katz <david.si.katz@gmail.com>
2022-03-29 09:40:20 -0400

    Initial commit

---
+++ template/Dockerfile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cat <<EOF > "${GIT_ROOT}/Dockerfile"
+FROM golang:${GO_VERSION} as build
+WORKDIR /go/src/app
+COPY go.mod go.sum ./
+RUN go mod download
+COPY . .
+RUN go build -o /go/bin/app
+
+FROM debian:buster-slim
+RUN apt-get update && apt-get install --yes ca-certificates
+RUN groupadd -r app && useradd --no-log-init -r -g app app
+USER app
+COPY --from=build /go/bin/app /
+ENV APP_ADDR ":${DEFAULT_APP_PORT}"
+EXPOSE ${DEFAULT_APP_PORT}
+ENTRYPOINT ["/app"]
+EOF
